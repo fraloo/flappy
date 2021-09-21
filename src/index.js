b1977a2aa806caa58baffdb34621acc3cea0126d
nilc <nilspett@gmail.com>
2021-09-21 13:40:16 +0200

    added tiger

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import Phaser from "phaser";
 import backgroundImg from "./assets/background.png";
 import birdImg from "./assets/bird.gif";
 import palmImg from "./assets/palm_tree.png";
+import tigerImg from "./assets/tiger.png"
 import cloudImg from "./assets/cloud.png";
 import jumpSound from "./assets/audio/jump.mp3";
 import gameoverSound from "./assets/audio/gameover.mp3";
 import smackSound from "./assets/audio/smack.mp3";
+const gamewidth = 1500;
+const gameheight = 800;
 class TheGame extends Phaser.Scene {
     constructor() {
         super({ key: "TheGame" });
@@ -16,6 +19,7 @@ class TheGame extends Phaser.Scene {
         this.load.image("background", backgroundImg);
         this.load.image("bird", birdImg);
         this.load.image("tree", palmImg);
+        this.load.image("tiger",tigerImg);
         this.load.image("cloud", cloudImg);
         this.load.audio('jump', jumpSound);
         this.load.audio('smack', smackSound);
@@ -27,30 +31,23 @@ class TheGame extends Phaser.Scene {
         this.cludRuns = 0
         this.cludSpeed = 5
 
-        this.scoreBoard = this.add.text(10, 10, '', { fill: '#00ff00' });
-
         // Background
-        var graphics = this.add.graphics();
+        this.paintBackground();
+        this.scoreBoard = this.add.text(10, 10, '', { fill: '#ffff00' });
 
-        graphics.fillStyle(0xffffff, 1);
-        graphics.fillRect(0, 0, 1000, 800);
-
-        const stripes = 8;
-
-        for (var i = 0; i < stripes; ++i) {
-            var color = 0x33ccff;
-            var alpha = 0.2 + ((i / stripes-1) * 0.8);
-            graphics.fillStyle(color, alpha);
-            graphics.fillRect(0, ((800 - 166)/stripes) * i , 1000, 256);
-        }
         
         // Tree
         this.tree = this.add
-            .sprite(800, 800 - (166 + 175), "tree")
+            .sprite(gameheight, gameheight - (166 + 175), "tree")
             .setOrigin(0, 0);
         this.physics.add.existing(this.tree, false);
         this.tree.body.setVelocity(-550, 0);
 
+        // Taiger
+
+        this.tiger= this.add.sprite(600,gameheight-300, "tiger").setOrigin(0,0);
+        this.physics.add.existing(this.tiger, false);
+        this.tiger.body.setVelocity(-1550, 0);
         // ClaoOoud
         this.clud = this.add.sprite(1200, 50, "cloud").setOrigin(0, 0);
         this.physics.add.existing(this.clud, true);
@@ -58,7 +55,7 @@ class TheGame extends Phaser.Scene {
 
         // Background
         this.background = this.add
-            .tileSprite(0, 800 - 166, 1000, 166, "background")
+            .tileSprite(0, gameheight - 166, gamewidth, 166, "background")
             .setOrigin(0, 0);
         this.physics.add.existing(this.background, true);
 
@@ -76,8 +73,11 @@ class TheGame extends Phaser.Scene {
         this.physics.add.collider(this.bird, this.background);
         this.physics.add.collider(this.tree, this.bird, ()=> this.gameOver());
         this.physics.add.collider(this.clud, this.bird, ()=> this.gameOver());
+        this.physics.add.collider(this.tiger,this.bird, () => this.gameOver())
         this.physics.add.collider(this.tree, this.background);
         this.physics.add.collider(this.clud, this.background);
+        this.physics.add.collider(this.tiger, this.background);
+        
         this.input.mouse.disableContextMenu();
 
         this.input.on('pointerup', (pointer) => {
@@ -91,6 +91,22 @@ class TheGame extends Phaser.Scene {
         
     }
 
+    paintBackground() {
+        var graphics = this.add.graphics();
+
+        var color = 0x33ccff;
+        graphics.fillStyle(0xffffff, 1);
+        graphics.fillRect(0, 0, gamewidth, gameheight);
+
+        const stripes = 9;
+
+        for (var i = 0; i < stripes; ++i) {
+            var alpha = 0.2 + ((i / stripes - 1) * 0.8);
+            graphics.fillStyle(color, alpha);
+            graphics.fillRect(0, ((gameheight - 166) / stripes) * i, gamewidth, 256);
+        }
+    }
+
     gameOver() {
         this.smack.play();
         this.scene.start("GameOver", {
@@ -102,17 +118,24 @@ class TheGame extends Phaser.Scene {
         this.background.tilePositionX += 4;
         // this.tree.body.setX(this.tree.x - 2);
         if (this.tree.x + this.tree.width < 0) {
-            this.tree.body.x = 1000;
+            this.tree.body.x = gamewidth;
             this.treeRuns+=5
             const newVelocity = randomIntFromInterval(-400, -700) + this.treeRuns;
             this.tree.body.setVelocity(newVelocity, 0);
             this.score++
         }
 
+        if (this.tiger.x + this.tiger.width < 0) {
+            this.tiger.body.x = gamewidth;
+            //this.treeRuns+=5
+            //const newVelocity = randomIntFromInterval(-400, -700) + this.treeRuns;
+            this.tiger.body.setVelocity(-gamewidth, -100);
+            this.score++
+        }
         
         this.clud.x = this.clud.body.x -= this.cludSpeed;
         if (this.clud.x + this.clud.width < 0) {
-            this.clud.body.x = this.clud.x = 1000;
+            this.clud.body.x = this.clud.x = gamewidth;
             this.score++
             this.cludRuns++
             this.cludSpeed = randomIntFromInterval(3, 10);
@@ -126,6 +149,7 @@ class TheGame extends Phaser.Scene {
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
 
 
 class GameOver extends Phaser.Scene {
@@ -146,7 +170,7 @@ class GameOver extends Phaser.Scene {
         var graphics = this.add.graphics();
 
         graphics.fillStyle(0xffffff, 1);
-        graphics.fillRect(0, 0, 1000, 800);
+        graphics.fillRect(0, 0, gamewidth, gameheight);
 
         const stripes = 8;
 
@@ -154,13 +178,13 @@ class GameOver extends Phaser.Scene {
             var color = 0x33ccff;
             var alpha = 0.2 + ((i / stripes-1) * 0.8);
             graphics.fillStyle(color, alpha);
-            graphics.fillRect(0, ((800 - 166)/stripes) * i , 1000, 256);
+            graphics.fillRect(0, ((gameheight - 166)/stripes) * i , gamewidth, 256);
         }
            
 
         // Background
         this.background = this.add
-            .tileSprite(0, 800 - 166, 1000, 166, "background")
+            .tileSprite(0, gameheight - 166, gamewidth, 166, "background")
             .setOrigin(0, 0);
         this.physics.add.existing(this.background, true);
 
@@ -209,8 +233,8 @@ class GameOver extends Phaser.Scene {
 const config = {
     type: Phaser.AUTO,
     parent: "phaser-example",
-    width: 1000,
-    height: 800,
+    width: gamewidth,
+    height: gameheight,
     scene: [TheGame, GameOver],
     physics: {
         default: "arcade",
